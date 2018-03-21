8830c1542f0aa8d498311a81dddcc5e9e1ce9572
gracekashe <gracekashe@gmail.com>
2018-03-21 11:34:40 +0300

    inserted code to js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@ var number2=parseInt(prompt("enter another number:"));
 var result=add(number1, number2);
 
 alert(result);
+
+var BMI=function(weight, height) {return weight / height;};
+
+ var weight=parseInt(prompt("enter weight:"));
+ var height=parseInt(prompt("enter another weight:"));
+
+ var result=BMI(weight, height);
+
+ alert(result / height);
+
+ var subtract=function(number1, number2) {return number1 - number2;};
+ var number1=parseInt(prompt("any number:"));
+ var number2=parseInt(prompt("another number:"));
+ var result=subtract(number1, number2);
+
+ alert(result);
+
+ var multiply=function(number1, number2) {return number1 * number2;};
+ var number1=parseInt(prompt("any number:"));
+ var number2=parseInt(prompt("enter number:"));
+ var result=multiply(number1, number2);
+
+ alert(result);
